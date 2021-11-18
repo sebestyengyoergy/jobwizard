2df8f761b3b3491d73957312a9376414e121c05a
rizwan.sarfraz <rizwan.sarfraaz@gmail.com>
2021-11-18 23:16:39 +0500

    switch language issue fixed

--- src/components/SwitchLanguage.vue
+++ src/components/SwitchLanguage.vue
@@ -1,11 +1,15 @@
 <template>
   <q-btn-dropdown no-caps color="primary">
     <template #label>
-      <img :src="'flags/'+$root.$i18n.locale+'.png'" width="28" :alt="$root.$i18n.locale.substr(0, 2).toUpperCase()" class="q-mr-md">
+      <img :src="'flags/'+$root.$i18n.locale+'.png'" width="28" :alt="$root.$i18n.locale.substr(0, 2).toUpperCase()"
+           class="q-mr-md"
+      >
       {{ $q.platform.is.mobile ? '' : $t('localeName') }}
     </template>
     <q-list>
-      <q-item v-for="(msg,lang) in $root.$i18n.messages" :key="lang" v-close-popup clickable class="items-center" @click="setLocale(lang)">
+      <q-item v-for="(msg,lang) in $root.$i18n.messages" :key="lang" v-close-popup clickable class="items-center"
+              @click="setLocale(lang)"
+      >
         <q-item-section side>
           <q-icon :color="lang === $root.$i18n.locale ? null : 'transparent'" name="mdi-check" />
         </q-item-section>
@@ -19,26 +23,36 @@
 import { SET_LANG } from '../store/names';
 import { mapMutations } from 'vuex';
 
-export default
-{
+export default {
   name: 'SwitchLanguage',
   methods:
-    {
-      ...mapMutations([SET_LANG]),
-      setLocale(lang)
       {
-        this.$root.$i18n.locale = lang;
-        this[SET_LANG](lang);
-
-        this.$router.push('/' + lang + (this.$route.name !== 'wizard' ? '/' + this.$route.name : ''));
-        import(
-          /* webpackInclude: /(de|en-GB)\.js$/ */
-          'quasar/lang/' + (lang === 'en' ? 'en-GB' : lang)
-        ).then(lang =>
+        ...mapMutations([SET_LANG]),
+        setLocale(lang)
         {
-          this.$q.lang.set(lang.default);
-        });
+          this.$root.$i18n.locale = lang;
+          this[SET_LANG](lang);
+
+          const routeName = this.$route.name;
+          let routePath = '/' + lang;
+          if (routeName !== 'wizard')
+          {
+            routePath = routePath + '/' + this.$route.name;
+            if (routeName === 'job')
+            {
+              routePath = routePath + '/' + this.$route.params.id;
+            }
+          }
+          this.$router.push(routePath);
+          //   this.$router.push('/' + lang + (this.$route.name !== 'wizard' ? '/' + this.$route.name : ''));
+          import(
+            /* webpackInclude: /(de|en-GB)\.js$/ */
+            'quasar/lang/' + (lang === 'en' ? 'en-GB' : lang)
+          ).then(lang =>
+          {
+            this.$q.lang.set(lang.default);
+          });
+        }
       }
-    }
 };
 </script>
