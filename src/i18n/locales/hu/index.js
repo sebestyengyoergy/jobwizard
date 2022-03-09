aeefaef9868c2f629b2d7ba247b353e95233203f
sebestyengyoergy <79409923+sebestyengyoergy@users.noreply.github.com>
2022-03-09 13:38:38 +0100

    Create index.js

---
+++ src/i18n/locales/hu/index.js
@@ -0,0 +1,98 @@
+export default
+{
+  localeName: () => 'Magyar',
+  jobs: () => 'Állások',
+  jobboard: () => 'Állásbörze',
+  job_title: () => 'Állásajánlat címe',
+  create_job: () => 'Új állásajánlat',
+  ad_management: () => 'Állásajánlatok kezelése',
+  public_jobs: () => 'Jelenlegi állásajánlatok',
+  settings: () => 'Beállítások',
+  statistics: () => 'Statisztika',
+  templates: () => 'Sablonok',
+  date: () => 'Dátum',
+  location: () => 'Hely',
+  company: () => 'Cég',
+  recruiter: () => 'Toborzó',
+  btn:
+  {
+    login: () => 'Bejelentkezés',
+    logout: () => 'Kijelentkezés',
+    close: () => 'Bezárás',
+    email: () => 'E-mail',
+    preview: () => 'Előnézet',
+    cancel: () => 'Mégse',
+    abort: () => 'Megszakítás',
+    back: () => 'Vissza',
+    continue: () => 'Következő',
+    download: () => 'Letöltés',
+    register: () => 'Regisztráció',
+    save: () => 'Mentés',
+    send: () => 'Elküldés',
+    publish: () => 'Közzététel'
+  },
+  nav:
+  {
+    settings: () => 'Beállítások',
+    jobs: () => 'Állások',
+    job_title: () => 'Állás neve',
+    misc: () => 'Egyebek',
+    organization: () => 'Szervezet',
+  },
+  help: {
+    dark_mode: () => 'Sötét kijelzés be- és kikapcsolása',
+    visibility: () => 'Munkabér elrejtése az álláshirdetésben',
+    reference: () => 'Hozzárendelhet az álláshirdetéshez egy hivatkozási számot, amire a jelentkezőnek hivatkoznia kell.',
+    apply_post: () => 'Itt megadhatja, hogy a pályázatokat, hagyományosan postai úton kéri-e beküldeni. A pályázat gomb az álláshirdetésben ehhez igazodik.'
+  },
+  label:
+  {
+    organization: () => 'Szervezet',
+    country: () => 'Ország',
+    countries: () => 'Országok',
+    location: () => 'Helység',
+    apply_url: () => 'Jelentkezés online',
+    apply_email: () => 'Jelentkezés e-mailben',
+    apply_post: () => 'Jelentkezés csak postai úton',
+    reference: () => 'Hivatkozás',
+    freelance: () => 'Szabadúszó',
+    contract: () => 'Szerződéses',
+    internship: () => 'Gyakornoki állás',
+    apprenticeship: () => 'Tanulói gyakorlat',
+    fulltime: () => 'Teljes munkaidő',
+    parttime: () => 'Részmunkaidő',
+    minijob: () => 'Minijob állás',
+    shiftwork: () => 'Több műszak',
+    work_duration: () => 'Munkaidő',
+    work_kind: () => 'Munka jellege',
+    visibility: () => 'Láthatóság',
+    salary: () => 'Éves bér',
+    homeoffice: () => 'Távmunka',
+  },
+  msg: {
+    job_saved_success: () => 'Az álláshirdetés sikeresen elmentve.',
+  },
+  rules:
+  {
+    required: () => 'Kötelező adat',
+    invalid_date: () => 'Érvénytelen dátum',
+    invalid_email:
+    {
+      delimiters: () => 'Az e-mail cím érvénytelennek tűnik (ellenőrizze a @ jelet és a pontokat)',
+      unicode_user: () => 'Az e-mail cím név részében érvénytelen betűk vannak',
+      unicode_domain: () => 'A doménnév érvénytelen betűket vagy jeleket tartalmaz',
+      username: () => 'Az e-mailben levő felhasználónév érvénytelennek tűnik',
+      ip: () => 'A cél IP cím érvénytelen!',
+      domain: () => 'A doménnév érvénytelennek tűnik',
+      hostname: () => 'Az e-mail címből hiányzik a kiszolgáló neve!',
+    },
+    invalid_url: () => 'Érvénytelen URL'
+  },
+  steps: {
+    stepOne: () => 'Általános adatok',
+    stepTwo: () => 'Álláshirdetés létrehozása',
+    stepThree: () => 'Kategóriák',
+    stepFour: () => 'Csatornák'
+  }
+
+};
