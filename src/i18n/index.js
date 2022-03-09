647c4c95941fa5cfb2c4a24f11046caecd16df52
sebestyengyoergy <79409923+sebestyengyoergy@users.noreply.github.com>
2022-03-09 13:41:59 +0100

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,13 @@
 import english from './locales/en';
 import german from './locales/de';
 import french from './locales/fr';
+import hungarian from '.locales/hu'
 
 const messages = {
   de: german,
   en: english,
-  fr: french
+  fr: french,
+  hu: magyar
 };
 
 export { messages };
